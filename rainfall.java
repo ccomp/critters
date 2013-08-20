a1f333863d5ec6cd4e67dfc904d604efe6f0d7cf
apcs <apcs@mvhs-imacu52804.dcsdk12.org>
2013-08-20 08:02:51 -0600

    Rainfall, broski.

---
+++ rainfall.java
@@ -0,0 +1,20 @@
+public class rainfall {
+	
+	public static void main(String[] args) {
+		
+		double april=12.0;
+		double may=14.0;
+		double june=8.0;
+
+		double ave=april+may+june;
+		double average=ave/3;
+
+		System.out.print("\tRainfall for April:  "+ april);
+		System.out.print("\n\tRainfall for May: " + may);
+		System.out.print("\n\tRainfall for June: " + june);	
+		System.out.println("\n\tAverage rainfall:  " +average);	
+
+
+	}
+
+}
